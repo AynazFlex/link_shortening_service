d4ab1be43e73c42d296cb92010552fcee2a53185
AynazFlex <jkz1713x2@gmail.com>
2022-09-12 17:37:10 +0300

    refactoring

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,19 +1,15 @@
 import { FC, useState } from "react";
-import { resetItems } from "../../store/statisticsReducer";
-import { useAppDispatch } from "../../store/store";
 import styles from "./Styles.module.scss";
 import Table from "./Table";
 import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
 const Stataistics: FC = () => {
   const [isOpen, setOpen] = useState(false);
-  const dispatch = useAppDispatch();
 
   return (
     <div className={styles.statistics}>
       <button
         onClick={() => {
-          isOpen && dispatch(resetItems());
           setOpen((prev) => !prev);
         }}
       >
--- src/components/Statistics/Table.tsx
+++ src/components/Statistics/Table.tsx
@@ -1,27 +1,21 @@
 import { FC, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { IRespSqueezy } from "../../api/api";
-import { getStatistics } from "../../store/statisticsReducer";
+import { getStatistics, resetItems } from "../../store/statisticsReducer";
 import { RootState, useAppDispatch } from "../../store/store";
 import Item from "./Item";
 
-const Table: FC = () => {
-  const offset = useRef<number>(0);
+interface IProps {
+  items: IRespSqueezy[];
+}
+
+const FilterForTable: FC<IProps> = ({ items }) => {
   const [id, setId] = useState("");
   const [short, setShort] = useState("");
   const [target, setTarget] = useState("");
   const [counter, setCounter] = useState("");
-  const [sortTag, setSortTag] = useState("");
-  const { items, isFetch } = useSelector(
-    (state: RootState) => state.statistics
-  );
-  const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(getStatistics(offset.current));
-  }, []);
-
-  const processingItems = () => {
+  const filterItems = () => {
     let array = [...items];
     if (id) array = array.filter((item) => String(item.id).includes(id));
     if (short)
@@ -34,6 +28,74 @@ const Table: FC = () => {
       );
     if (counter)
       array = array.filter((item) => String(item.counter).includes(counter));
+    return array;
+  };
+
+  return (
+    <tbody>
+      <tr>
+        <td>
+          <input
+            placeholder="filter"
+            value={id}
+            onChange={({ target: { value } }) => {
+              setId(value);
+            }}
+          />
+        </td>
+        <td>
+          <input
+            placeholder="filter"
+            value={short}
+            onChange={({ target: { value } }) => {
+              setShort(value);
+            }}
+          />
+        </td>
+        <td>
+          <input
+            placeholder="filter"
+            style={{ width: "300px" }}
+            value={target}
+            onChange={({ target: { value } }) => {
+              setTarget(value);
+            }}
+          />
+        </td>
+        <td>
+          <input
+            placeholder="filter"
+            value={counter}
+            onChange={({ target: { value } }) => {
+              setCounter(value);
+            }}
+          />
+        </td>
+      </tr>
+      {filterItems().map((item) => (
+        <Item key={item.id} {...item} />
+      ))}
+    </tbody>
+  );
+};
+
+const Table: FC = () => {
+  const offset = useRef<number>(0);
+  const [sortTag, setSortTag] = useState("");
+  const { items, isFetch } = useSelector(
+    (state: RootState) => state.statistics
+  );
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getStatistics(offset.current));
+    return () => {
+      dispatch(resetItems());
+    };
+  }, []);
+
+  const sortItems = () => {
+    let array = [...items];
     sortTag === "id" &&
       array.sort((a: IRespSqueezy, b: IRespSqueezy) => a.id - b.id);
     sortTag === "short" &&
@@ -81,50 +143,7 @@ const Table: FC = () => {
                 </th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <input
-                    placeholder="filter"
-                    value={id}
-                    onChange={({ target: { value } }) => {
-                      setId(value);
-                    }}
-                  />
-                </td>
-                <td>
-                  <input
-                    placeholder="filter"
-                    value={short}
-                    onChange={({ target: { value } }) => {
-                      setShort(value);
-                    }}
-                  />
-                </td>
-                <td>
-                  <input
-                    placeholder="filter"
-                    style={{ width: "300px" }}
-                    value={target}
-                    onChange={({ target: { value } }) => {
-                      setTarget(value);
-                    }}
-                  />
-                </td>
-                <td>
-                  <input
-                    placeholder="filter"
-                    value={counter}
-                    onChange={({ target: { value } }) => {
-                      setCounter(value);
-                    }}
-                  />
-                </td>
-              </tr>
-              {processingItems().map((item) => (
-                <Item key={item.id} {...item} />
-              ))}
-            </tbody>
+            <FilterForTable items={sortItems()} />
           </table>
         ) : (
           <div>No links</div>
